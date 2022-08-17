bad04e239a9c04a24a76ededc268283d96ae677d
umid2002 <umidhusanov03@bk.ru>
2022-08-18 04:07:03 +0500

    bug fixed

--- src/pages/not-found/not-found.js
+++ src/pages/not-found/not-found.js
@@ -1,9 +1,18 @@
+import Container from "../../components/container/container";
 import "./not-found.scss"
-
+import notFound from "../../assets/img/not-found.png"
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <h2 className="not-found">Not Found - 404</h2>
+    <Container>
+      <div className="not-found">
+        <img className="not-found__img" src={notFound} alt="Img" />
+        <h2 className="not-found__title">There is no feedback yet.</h2>
+        <p className="not-found__subtitle">Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
+        <Link className="home-link" to="/">Home</Link>
+      </div>
+    </Container>
   );
 }
 export default NotFound;
